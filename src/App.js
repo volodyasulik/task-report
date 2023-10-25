081623a7406c4e488806bd179405733303f898ff
Volodymyr <volodasulik@gmail.com>
2023-10-25 22:52:21 +0300

    feat: Fixed lazy load

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { Link, Routes, BrowserRouter, Route, Navigate } from 'react-router-dom'
-import dataJson from './data.json'
-import DummyChart from './DummyChart';
-import DummyList from './DummyList';
-import DummyTable from './DummyTable';
+import React, { Suspense, lazy } from 'react';
+import { Link, Routes, BrowserRouter, Route, Navigate } from 'react-router-dom';
+import dataJson from './data.json';
+
+const DummyTable = lazy(() => import('./DummyTable'));
+const DummyList = lazy(() => import('./DummyList'));
+const DummyChart = lazy(() => import('./DummyChart'));
 
 function App() {
     return (
         <div>
-
             <BrowserRouter>
-
-                <React.Suspense >
+                <React.Suspense fallback={<div>Loading...</div>}>
                     {dataJson.map(({ id, title, path }) => <LinkComponent key={id} title={title} path={path} />)}
                     <Routes>
-                        <Route path={'dummyTable'} element={<DummyTable />} />
-                        <Route path={'dummyChart'} element={<DummyChart />} />
-                        <Route path={'dummyList'} element={<DummyList />} />
+                        <Route path={'dummyTable'} element={<Suspense fallback={<div>Loading...</div>}><DummyTable /></Suspense>} />
+                        <Route path={'dummyChart'} element={<Suspense fallback={<div>Loading...</div>}><DummyChart /></Suspense>} />
+                        <Route path={'dummyList'} element={<Suspense fallback={<div>Loading...</div>}><DummyList /></Suspense>} />
                         <Route path={'*'} element={<Navigate to={dataJson[0].path} />} />
                     </Routes>
                 </React.Suspense>
@@ -26,7 +25,7 @@ function App() {
 }
 
 function LinkComponent(props) {
-    return <Link to={props.path}>{props.title}</Link>
+    return <Link to={props.path}>{props.title}</Link>;
 }
 
 export default App;
